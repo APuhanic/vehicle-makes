a26a08cbf19456ce77a9253ea7dd167e9638b5b6
Antonio Puhanic <puhanicantonio@gmail.com>
2024-05-07 11:46:03 +0200

    Initial commit. Modified README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# vehicle_makes
+
+Flutter project that uses [Car API](https://carapi.app/api) for vehicle makes.
---
+++ android/app/src/main/kotlin/com/example/vehicle_makes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vehicle_makes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
